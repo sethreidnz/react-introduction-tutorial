c820e8d0a6e12ba7d99b2716a9ab422c7753b361
justsayno <contact@sethreid.co.nz>
2016-08-02 21:30:02 +1200

    updated

--- src/api/locations.js
+++ src/api/locations.js
@@ -16,27 +16,27 @@ const locations =  [
             square: "src/images/nugget-point-lighthouse-rect.jpg",
             rectangle: "src/images/nugget-point-lighthouse-sq.jpg"
         },
-        shortDescription:"The Waikato River, New Zealand's longest river, moves gracefully north from Lake Taupo between banks 100 metres apart.",
+        shortDescription:"Nugget Point is one of the most distinctive landforms along the Otago coast. It's a steep headland...",
         longDescription:"<p> Nugget Point is one of the most distinctive landforms along the Otago coast. It's a steep headland with a lighthouse and a scattering of rocky islets (The Nuggets). A five minute walk takes you to the lighthouse, where you'll enjoy amazing views along the coastline in both directions. If you look down onto the rocks directly below the lighthouse, you'll see fur seals lolling about, playing in rock pools and generally having fun. Bring binoculars for close-up viewing of this breeding colony. </p><p> Nugget Point is also home to sea lions, sooty shearwaters, shags, yellow-eyed penguins, spoonbills and a breeding colony of gannets. Occasionally, elephant and leopard seals visit the area. If you watch the water for a while, you might see Hector's dolphins.From Nugget Point it's an easy walk to Roaring Bay, where you can hide and wait for yellow-eyed penguins. Early morning or sunset are the best times for viewing. </p>"
     },
     {
         id: "3",
         name:"Te Mata Peak",
         images: {
-            square: "src/images/single-te-mata-peak-rect.jpg",
-            rectangle: "src/images/single-te-mata-peak-sq.jpg"
+            square: "src/images/te-mata-peak-rect.jpg",
+            rectangle: "src/images/te-mata-peak-sq.jpg"
         },
-        shortDescription:"The Waikato River, New Zealand's longest river, moves gracefully north from Lake Taupo between banks 100 metres apart.",
+        shortDescription:"Te Mata Peak is at the western boundary of the wine-producing Heretaunga Plains and stands nearly 400 metres above sea level.",
         longDescription:"<p> Te Mata Peak is at the western boundary of the wine-producing Heretaunga Plains and stands nearly 400 metres above sea level. From the summit of the peak you can enjoy panoramic views of the Ruahine, Kaweka and Maungaharuru Ranges and Cape Kidnappers. The volcano Ruapehu, in the centre of Tongariro National Park, is also visible on a clear day. </p><p> The nature trails on Te Mata Peak are great for hikers and mountain bikers. It's an excellent cardio workout going up, but the huge views make the exertion totally worthwhile. Other hiking and biking trails in the Te Mata Trust Park lead through forest and along limestone valleys. </p>"
     },
     {
         id: "4",
         name:"Aratiatia Rapids",
         images: {
-            square: "src/mages/aratiatia-rapids-rect.jpg",
+            square: "src/images/aratiatia-rapids-rect.jpg",
             rectangle: "src/images/aratiatia-rapids-sq.jpg"
         },
-        shortDescription:"The Waikato River, New Zealand's longest river, moves gracefully north from Lake Taupo between banks 100 metres apart.",
+        shortDescription:"A few times each day, spill gates from a dam are opened at the top of the Aratiatia rapids and the narrow gorge fills...",
         longDescription:"<p> A few times each day, spill gates from a dam are opened at the top of the Aratiatia rapids and the narrow gorge fills with turbulent water surging past at up to 90,000 litres per second. Then, as the gates are closed about thirty minutes later, the turbulence recedes to a placid stream. By seeing the transition to full-flow and back again, visitors can fully appreciate the power of nature. </p><p> At Aratiatia the Waikato River falls naturally through 28 metres in the space of one kilometre. This natural drop has been harnessed for environmentally-sustainable hydroelectric power - headwaters are diverted through a tunnel to the power station. Each day at 10am, 12 noon and 2pm - and also at 4pm in summer - the flow through the Aratiatia rapids is allowed to resume its natural rate. There are several excellent vantage points on the high rock bluffs that dominate this turbulent stretch of the river. </p>"
     }
 ];
