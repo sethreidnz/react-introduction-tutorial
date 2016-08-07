c07c30fefc6e549c59bb85de6f7c83f2700849e5
justsayno <contact@sethreid.co.nz>
2016-08-07 23:38:41 +1200

    added basic component

--- src/containers/EmployeeProfile.js
+++ src/containers/EmployeeProfile.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react'
-import getEmployee from '../api/employees'
 
 class EmployeeProfile extends Component {
-    constructor(){
-        this.state = {
-            employee: getEmployee(1)
-        }
-    }
     render(){
         return (
             <div>
