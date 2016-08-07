7fd770ffa4933a56b2e28239e917b503e3c7f957
justsayno <contact@sethreid.co.nz>
2016-08-07 16:32:19 +1200

    made example even more simple

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 
 ReactDOM.render(
-  <App />,
+  <div>
+    Hello World!
+  </div>,
   document.getElementById('root')
 )
