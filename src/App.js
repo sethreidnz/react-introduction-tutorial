99ddfeec1e4cb6e302a80abe752085c474892aeb
justsayno <contact@sethreid.co.nz>
2016-08-07 17:04:41 +1200

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.props.message}
+        Hello World!
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Components
+import App from './App'
+
 ReactDOM.render(
-  <div>
-    Hello World!
-  </div>,
+  <App />,
   document.getElementById('root')
 )
