3cc203f608fc5fa92adad98b84207eaebdeef7b8
justsayno <contact@sethreid.co.nz>
2016-08-07 21:07:19 +1200

    all but css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Main app wrapper component
+import App from './App'
+
 ReactDOM.render(
-  <div>
-    Hello World!
-  </div>,
+  <App />,
   document.getElementById('root')
 )
