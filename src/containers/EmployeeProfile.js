f77099544b107d6632b3dba863f2547fb814929d
justsayno <contact@sethreid.co.nz>
2016-08-08 00:02:13 +1200

    finished basic profile

--- src/containers/EmployeeProfile.js
+++ src/containers/EmployeeProfile.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
+import { getEmployee } from '../api/employees'
 
 class EmployeeProfile extends Component {
+    constructor({params}){
+        super()
+        
+        this.state = {
+            employee: getEmployee(params.employeeId)
+        }
+    }
     render(){
+        const { employee: { firstName, lastName, role, team, biography, avatar, keySkills, recentProjects } } = this.state
+        debugger
         return (
             <div>
                 <div className="col s12 m4">
                     <div className="row">
                         <div className="profile">
                             <div className="col s12 m6 profile-picture">
-                                <img src="/src/images/gabriella-boon.png" alt="" />
+                                <img src={avatar} alt="" />
                             </div>
                             <div className="col s12 m6 profile-details">
-                                <h5 className="profile-name">Gabriella Boon</h5>
+                                <h5 className="profile-name">{firstName} {lastName}</h5>
                                 <label>Role</label>
-                                <span className="profile-role">.NET developer</span>
+                                <span className="profile-role">{role}</span>
                                 <label>Team</label>
-                                <span className="profile-team">Modern Apps</span>
+                                <span className="profile-team">{team}</span>
                             </div>
                         </div>           
                     </div>
@@ -24,12 +34,9 @@ class EmployeeProfile extends Component {
                             <div className="col s12 m12 profile-details">
                                 <h5 className="profile-name">Key Skills and Technologies</h5>
                                 <ul className="collection">
-                                    <li className="collection-item">MCV / WEBAPI</li>
-                                    <li className="collection-item">SQL</li>
-                                    <li className="collection-item">Epi Server</li>
-                                    <li className="collection-item">Azure Configuration</li>
-                                    <li className="collection-item">Web Forms</li>
-                                    <li className="collection-item">Xamarin</li>
+                                    {keySkills.map((skill) => (
+                                        <li className="collection-item">{skill.name}</li>
+                                    ))}
                                 </ul>
                             </div>
                         </div>           
@@ -38,17 +45,13 @@ class EmployeeProfile extends Component {
             <div className="col s12 m8">
                 <h5>Biography</h5>
                 <p>
-                    Gabriella Boon has been a .NET developer since 2011 having build various soultions
-                    using ASPNET as well as mobile development using Xamarin. With background mostly
-                    in server side development Gabriella is in her element when developing de-coupled
-                    services and architecting SQL database solutions.
+                    {biography}
                 </p>
                 <h5>Recent Projects</h5>
                 <ul className="collection">
-                    <li className="collection-item">City Council - Mobility solution</li>
-                    <li className="collection-item">Mircosoft - Showcase project</li>
-                    <li className="collection-item">Azure SQL database migraiton - On premise to cloud</li>
-                    <li className="collection-item">Craig and Smiths - Farming supplies web site</li>
+                    {recentProjects.map((project) => (
+                        <li className="collection-item">{project.name}</li>
+                    ))}
                 </ul>
             </div>
         </div>
