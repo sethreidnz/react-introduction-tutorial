415ed53544e22890e4dfae8342fcedccb3380175
justsayno <contact@sethreid.co.nz>
2016-08-08 19:18:15 +1200

    added props

--- src/components/EmployeeListItem.js
+++ src/components/EmployeeListItem.js
@@ -15,7 +15,21 @@ const EmployeeListItem = ({employee}) => (
 )
 
 EmployeeListItem.propTypes = { 
-    employee: PropTypes.object.isRequired
+    employee: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        firstName: PropTypes.string.isRequired,
+        lastName: PropTypes.string.isRequired,
+        avatar: PropTypes.string.isRequired,
+        role: PropTypes.string.isRequired,
+        team: PropTypes.string.isRequired,
+        biography: PropTypes.string.isRequired,
+        keySkills: PropTypes.arrayOf(React.PropTypes.shape({
+            name: PropTypes.string.isRequired
+        })),
+        recentProjects: PropTypes.arrayOf(React.PropTypes.shape({
+            name: PropTypes.string.isRequired
+        }))
+    })
 }
 
 export default EmployeeListItem
