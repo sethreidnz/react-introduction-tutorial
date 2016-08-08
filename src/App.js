371f3531db957da75d19288c97701585ae320ef9
justsayno <contact@sethreid.co.nz>
2016-08-08 18:42:08 +1200

    fixed dash

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ class App extends Component {
   render() {
     const { children } = this.props
     return (
-      <div className="App">
+     <div className="App">
         <nav>
-    <div className="nav-wrapper black ">
-       <div className="container">
-        <a href="/static" className="brand-logo">Employee CV Manager</a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li className="active"><a href="#">Employees</a></li>
-          <li><a href="#">My Profile</a></li>
-          <li><a href="#">Settings</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-    <div className="container main-content">      
-     <div className="row">
-            { children }
-      </div>
-    </div>
+          <div className="nav-wrapper black ">
+            <div className="container">
+              <a href="/static" className="brand-logo">Employee CV Manager</a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li className="active"><a href="#">Employees</a></li>
+                <li><a href="#">My Profile</a></li>
+                <li><a href="#">Settings</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+          <div className="container main-content">      
+            <div className="row">
+                  { children }
+            </div>
+          </div>
       </div>
     )
   }
