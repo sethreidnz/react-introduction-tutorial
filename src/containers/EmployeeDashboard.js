e6896098c89e658c1f9472305d4d5a931f32bb72
justsayno <contact@sethreid.co.nz>
2016-08-08 18:39:14 +1200

    fixed dash

--- src/containers/EmployeeDashboard.js
+++ src/containers/EmployeeDashboard.js
@@ -8,7 +8,7 @@ import EmployeeListItem from '../components/EmployeeListItem'
 class EmployeeDashboard extends Component {
   render() {
     return (
-      <div className="employee-dashboard">
+      <div className="employee-dashboard col s12 m7">
             <EmployeeList>
              {employees.map((employee) => {
                 return <EmployeeListItem key={employee.id} employee={employee} />
