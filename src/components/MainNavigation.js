ad83937ae344e7ad488936f8198fc4855c366086
justsayno <contact@sethreid.co.nz>
2016-08-13 11:32:37 +1200

    added main nav

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class MainNavigation extends Component{
+    render(){
+         return (
+            <nav>
+                <div className="nav-wrapper black">
+                    <div className="container">
+                        <a href="#!" className="brand-logo">Employee CV Manager</a>
+                        <a href="#" data-activates="MainNavigationCollapse" className="button-collapse">
+                            <i className="material-icons">menu</i>
+                        </a>
+                        <ul className="main-navigation right hide-on-med-and-down">
+                            <li className="active"><a href="#">Employees</a></li>
+                            <li><a href="#">My Profile</a></li>
+                            <li><a href="#">Settings</a></li>
+                        </ul>
+                        <ul className="side-nav" id="MainNavigationCollapse">
+                            <li className="active"><a href="#">Employees</a></li>
+                            <li><a href="#">My Profile</a></li>
+                            <li><a href="#">Settings</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )    
+    }
+}
+
+export default MainNavigation
