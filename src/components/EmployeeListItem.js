bbb7f4e6a41b9f2af793a11b7c76a74c39c02485
hiaw <danielgq.chong@gmail.com>
2016-08-25 12:34:33 +1200

    make sure PropTypes error doesn't show up (#6)

--- src/components/EmployeeListItem.js
+++ src/components/EmployeeListItem.js
@@ -1,14 +1,14 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 
 const EmployeeListItem = ({employee}) => (
-    <li className="collection-item avatar">
-        <img src={employee.avatar} alt={employee.name} className="circle" />
-        <span className="title">{employee.firstName} {employee.lastName}</span>
-        <p>{employee.role} <br/>
+    <li className='collection-item avatar'>
+        <img src={employee.avatar} alt={employee.name} className='circle' />
+        <span className='title'>{employee.firstName} {employee.lastName}</span>
+        <p>{employee.role} <br />
         {employee.team}
         </p>
-        <a href="#" className="secondary-content btn black">
-            <i className="material-icons left">description</i>
+        <a href='#' className='secondary-content btn black'>
+            <i className='material-icons left'>description</i>
             View C.V.
         </a>
     </li>
